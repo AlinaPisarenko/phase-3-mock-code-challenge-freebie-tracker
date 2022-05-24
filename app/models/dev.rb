6445a64155dbee66735ac1eb217ad9e5e3dcc944
Alina <pisarenko.alina@gmail.com>
2022-05-24 11:27:06 -0400

    mock

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,15 @@
 class Dev < ActiveRecord::Base
+has_many :freebies
+has_many :companies, through: :freebies
+
+def received_one?(item_name)
+self.freebies.exists?(item_name: item_name)
+end
+
+def give_away(dev, freebie)
+    if(self.received_one?(freebie.item_name))
+Freebie.where(id: freebie.id).update(dev_id: dev.id )
+    end
+end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,15 @@ Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
+Freebie.create(value: 5, item_name: "bag", company_id: 2,dev_id: 4)
+Freebie.create(value: 10, item_name: "speaker", company_id: 1,dev_id: 4)
+Freebie.create(value: 4, item_name: "notepad", company_id: 1,dev_id: 2)
+Freebie.create(value: 8, item_name: "charger", company_id: 3,dev_id: 1)
+Freebie.create(value: 3, item_name: "pen", company_id: 4,dev_id: 1)
+Freebie.create(value: 6, item_name: "mug", company_id: 2,dev_id: 3)
+Freebie.create(value: 5, item_name: "water bottle", company_id: 2,dev_id: 1)
+
+
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
